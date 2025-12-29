d191ab2276b4d106b0d1b871811b795c0a617f31
Sanmati Patil <sanmatipatil1020@gmail.com>
2025-12-29 19:40:22 +0530

    result file added

---
+++ utils/result.js
@@ -0,0 +1,13 @@
+function createresult(error,data){
+ const result={}
+ if(data){
+    result.status="success"
+    result.data=data
+ }
+ else{
+    result.status="error"
+    result.error=error
+ }
+ return result
+}
+module.exports={createresult}
